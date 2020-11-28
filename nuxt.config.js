aee974fc8b6e3f2796c91f3d5cf9acc4ac61057c
Victor Vernilli <victorvernilli@Victors-MacBook-Pro.local>
2020-11-28 21:17:13 +0000

    Adjust favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@ export default {
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/favicon/favicon.ico', sizes: '48x48' },
     ],
   },
 
