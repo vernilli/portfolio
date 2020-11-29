7df03ff5c88093c57b0171b90d77800d710079dd
Victor Fernandes Vernilli <vfv@cin.ufpe.br>
2020-11-29 00:32:03 +0000

    Added footer to project (#4)
    Co-authored-by: Victor Vernilli <victorvernilli@Victors-MacBook-Pro.local>

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,18 @@
   <div>
     <app-navbar />
     <nuxt />
+    <app-footer />
   </div>
 </template>
 
 <script>
 import AppNavbar from '~/components/AppNavbar.vue'
+import AppFooter from '~/components/AppFooter.vue'
 
 export default {
   components: {
     AppNavbar,
+    AppFooter,
   },
 }
 </script>
