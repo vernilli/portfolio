74d75fc51405769ebd40c7454629d650aada2b6a
Victor Vernilli <victorvernilli@Victors-MacBook-Pro.local>
2020-11-29 19:59:52 +0000

    Added button component to project

---
+++ components/VvButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <nuxt-link
+    :to="path"
+    :class="[
+      fullWidth ? 'w-100' : '',
+      `vv-button ${buttonSize}`,
+    ]"
+    target="_blank"
+  >
+    <slot name="iconBefore"></slot>
+    <span class="ml-1 mr-1">
+      {{ text }}
+    </span>
+    <slot name="iconAfter"></slot>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: "vv-button",
+  props: {
+    text: String,
+    path: {
+      type: String,
+      required: true,
+    },
+    buttonSize: {
+      type: String,
+      default: 'md',
+    },
+    fullWidth: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
