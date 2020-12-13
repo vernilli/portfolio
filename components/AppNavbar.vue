4b0a0e3714022d566a2205508373b28e67584573
Victor Vernilli <victorvernilli@gmail.com>
2020-12-13 22:53:44 +0000

    Fix: heaver and footer fixes

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -7,59 +7,65 @@
       :class="{'on-top': topPage}"
     >
       <div class="container d-flex align-items-center justify-content-between">
-        <div class="header__title">
+        <div class="header__title d-block d-md-none">
+          <nuxt-link :to="language === 'pt-br' ? '/pt-br' : '/'">
+            {{ titleMobile }}
+          </nuxt-link>
+        </div>
+        <div class="header__title d-none d-md-block">
           <nuxt-link :to="language === 'pt-br' ? '/pt-br' : '/'">
             {{ title }}
           </nuxt-link>
         </div>
 
-        <transition name="fade" mode="out-in">
-          <!-- ENGLISH VERSION DESKTOP -->
-          <nav 
-            v-if="language == 'en'"
-            class="header__navigation d-none d-lg-flex"
-            :key="language"
-          >
-            <nuxt-link to="/" class="main-link ml-md-4">
-              {{ text.projects.en }}
-            </nuxt-link>
-            <nuxt-link to="/about" class="main-link ml-md-4">
-              {{ text.about.en }}
-            </nuxt-link>
-          </nav>
-          <!-- PORTUGUESE VERSION DESKTOP -->
-          <nav 
-            v-else
-            class="header__navigation d-none d-lg-flex"
-            :key="language"
-          >
-            <nuxt-link to="/pt-br" class="main-link ml-md-4">
-              {{ text.projects.pt }}
-            </nuxt-link>
-            <nuxt-link to="/pt-br/sobre" class="main-link ml-md-4">
-              {{ text.about.pt }}
-            </nuxt-link>
-          </nav>
-        </transition>
+        <div class="d-flex align-items-center">
+          <transition name="fade" mode="out-in">
+            <!-- ENGLISH VERSION DESKTOP -->
+            <nav 
+              v-if="language == 'en'"
+              class="header__navigation d-flex"
+              :key="language"
+            >
+              <nuxt-link to="/" class="main-link mr-2 mr-md-0 ml-md-4">
+                {{ text.projects.en }}
+              </nuxt-link>
+              <nuxt-link to="/about" class="main-link mr-2 mr-md-0 ml-md-4">
+                {{ text.about.en }}
+              </nuxt-link>
+            </nav>
+            <!-- PORTUGUESE VERSION DESKTOP -->
+            <nav 
+              v-else
+              class="header__navigation d-flex"
+              :key="language"
+            >
+              <nuxt-link to="/pt-br" class="main-link mr-2 mr-md-0 ml-md-4">
+                {{ text.projects.pt }}
+              </nuxt-link>
+              <nuxt-link to="/pt-br/sobre" class="main-link mr-2 mr-md-0 ml-md-4">
+                {{ text.about.pt }}
+              </nuxt-link>
+            </nav>
+          </transition>
 
-        <div 
-          id="burger-icon"
-          class="d-block d-md-none"
-          :class="{'open': burgerOpen}"
-          @click="toggleBurger"
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+          <div 
+            id="burger-icon"
+            class="d-block d-md-none"
+            :class="{'open': burgerOpen}"
+            @click="toggleBurger"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
       </div>
       
-      <transition name="fade">
-        <!-- ENGLISH VERSION MOBILE -->
+      <!-- <transition name="fade">
         <nav 
           v-if="burgerOpen && language == 'en'"
-          class="header__navigation container d-flex d-lg-none flex-column pt-3 pb-4"
+          class="header__navigation container d-flex d-md-none flex-column pt-3 pb-4"
           :key="language"
         >
           <nuxt-link to="/" class="d-inline mb-4">
@@ -69,10 +75,9 @@
             {{ text.about.en }}
           </nuxt-link>
         </nav>
-        <!-- PORTUGUESE VERSION MOBILE -->
         <nav 
           v-if="burgerOpen && language == 'pt-br'" 
-          class="header__navigation container d-flex d-lg-none flex-column pt-3 pb-4"
+          class="header__navigation container d-flex d-md-none flex-column pt-3 pb-4"
           :key="language"
         >
           <nuxt-link to="/pt-br" class="d-inline mb-4">
@@ -82,7 +87,7 @@
             {{ text.about.pt }}
           </nuxt-link>
         </nav>
-      </transition>
+      </transition> -->
 
       <div v-if="burgerOpen" class="header__lang-switcher d-flex d-md-none justify-content-center">
         <nuxt-link 
@@ -139,6 +144,7 @@ export default {
   data() {
     return {
       title: "Victor Vernilli",
+      titleMobile: "Vernilli",
       language: "en",
       pagePath: "/",
       languages: {
