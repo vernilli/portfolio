d423a9a158692d42cc3d87a7b94d4b2bdb995e99
Victor Vernilli <victorvernilli@gmail.com>
2021-02-14 23:34:14 +0000

    Created project pages

---
+++ pages/inlocomedia.vue
@@ -0,0 +1,94 @@
+<template>
+  <transition name="fade" mode="out-in">
+    <div :key="currentLocale" class="project-page">
+      <div class="project-page__hero desktop-image mb-4">
+        <img :src="require(`~/static/img/portfolio/${$t('projects[3].heroImg')}`)" alt="" class="w-100 h-100">
+        <h1 class="project-page__title text-center">
+          {{ $t('projects[3].projectTitle') }}
+        </h1>
+      </div>
+
+      <main>
+        <project-page-nav 
+          :anchors="itemsPageNav"
+          :activeItem="visibleItemNav"
+        />
+
+        <project-page-main
+          :content="$t('projects[3].projectContent', { returnObjects: true })"
+          @heading-visible-id="activeItemChanged"
+        />
+      </main>
+
+      <div class="project-page__other-projects container">
+        <h2 class="text-center">
+          {{ $t('otherProjectsCopy') }}
+        </h2>
+        <portfolio-showcase :hideProject="$t('projects[3].permalink')" />
+      </div>
+      
+    </div>
+  </transition>
+</template>
+
+<script>
+import VvButton from '~/components/VvButton.vue'
+import ProjectPageNav from '~/components/ProjectPageNav.vue'
+import ProjectPageMain from '~/components/ProjectPageMain.vue'
+import PortfolioShowcase from '~/components/PortfolioShowcase.vue'
+
+export default {
+  // layout: 'basic',
+  components: {
+    VvButton,
+    ProjectPageNav,
+    ProjectPageMain,
+    PortfolioShowcase,
+  },
+  props: {
+    visibleItemNav: String,
+  },
+  computed: {
+    currentLocale () {
+      return this.$i18n.locale
+    },
+    pageContent: function() {
+      return this.$i18n.messages[this.$i18n.locale].projects[3].projectContent
+    },
+    itemsPageNav: function() {
+      let ids = []
+      for(let i = 0; i < this.pageContent.length ; i++) {
+        if(this.pageContent[i].id) {
+          let item = {
+            id: this.pageContent[i].id,
+            heading: this.pageContent[i].heading,
+          }
+          ids.push(item)
+        }
+
+        if(this.pageContent[i].subsections) {
+          for(let j = 0; j < this.pageContent[i].subsections.length ; j++) {
+            if(this.pageContent[i].subsections[j].id) {
+              let item = {
+                id: this.pageContent[i].subsections[j].id,
+                heading: this.pageContent[i].subsections[j].heading,
+                subsection: true,
+              }
+              ids.push(item)
+            }
+          }
+        }
+      }
+      return ids
+    }
+  },
+  methods: {
+    activeItemChanged: function(el) {
+      this.visibleItemNav = el
+    },
+  },
+  mounted() {
+    this.visibleItemNav = this.$i18n.messages[this.$i18n.locale].projects[3].visibleItemNav
+  }
+}
+</script>
